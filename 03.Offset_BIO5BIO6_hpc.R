ded737fd025adfe2b73ab14a493bcbadc0fc3359
XiaquZ <xiaqu.zhou@gmail.com>
2024-10-25 17:58:07 +0200

    Reorder scripts name. Make linear regression model between offset and macroclimate by using TerraClimate data.

--- 03.Offset_BIO5BIO6_hpc.R
+++ 03.Offset_BIO5BIO6_hpc.R
@@ -1,6 +1,4 @@
 library(terra)
-library(tibble)
-library(ggplot2)
 
 # Load ForestClim BIO5 BIO6 2000-2020.
 microbio5 <- rast("/lustre1/scratch/348/vsc34871/input/ForestClim_05.tif")
@@ -8,8 +6,8 @@ microbio6 <- rast("/lustre1/scratch/348/vsc34871/input/ForestClim_06.tif")
 microbio5
 
 # Load resampled macroclimate BIO5 and 6, 1981-2010.
-macrobio5 <- rast("/lustre1/scratch/348/vsc34871/output/1981-2010chelsaBIO5_EUforests_25m.tif")
-macrobio6 <- rast("/lustre1/scratch/348/vsc34871/output/1981-2010chelsaBIO6_EUforests_25m.tif")
+macrobio5 <- rast("/lustre1/scratch/348/vsc34871/output/2000-2020terraBIO5_EUforests_25m.tif")
+macrobio6 <- rast("/lustre1/scratch/348/vsc34871/output/2000-2020terraBIO6_EUforests_25m.tif")
 macrobio5
 macrobio6
 plot(macrobio5)
@@ -18,13 +16,13 @@ plot(macrobio5)
 offsetbio5 <- microbio5 - macrobio5
 offsetbio5
 writeRaster(offsetbio5,
-    filename = "/lustre1/scratch/348/vsc34871/output/OffsetBIO5_Current.tif",
+    filename = "/lustre1/scratch/348/vsc34871/output/OffsetBIO5_2000-2020.tif",
     overwrite = TRUE
 )
 
 offsetbio6 <- microbio6 - macrobio6
 offsetbio6
 writeRaster(offsetbio6,
-    filename = "/lustre1/scratch/348/vsc34871/output/OffsetBIO6_Current.tif",
+    filename = "/lustre1/scratch/348/vsc34871/output/OffsetBIO6_2000-2020.tif",
     overwrite = TRUE
 )
--- 04.LinearRegressionMicroMacro.R
+++ 04.LinearRegressionMicroMacro.R
@@ -7,12 +7,12 @@ library(ggplot2)
 ###########################
 
 # Load offset and macroclimate data of BIO5.
-offsetbio5 <- rast("E:/Output/ForestOffset/BIO5BIO6/OffsetBIO5_Current.tif")
+offsetbio5 <- rast("E:/Input/TerraClimate/terraOffsetBIO5_2000-2020.tif")
 macrobio5 <- rast(
-  "E:/Input/CHELSAdata/BIO5BIO6/1981-2010/1981-2010chelsaBIO5_EUforests_25m.tif"
+  "E:/Input/TerraClimate/2000-2020terraBIO5_EUforests_25m.tif"
 )
 plot(offsetbio5, main = "OffsetBIO5 1981-2010")
-plot(macrobio5, main = "CHELSA BIO5 1981-2010")
+plot(macrobio5, main = "Terra BIO5 1981-2010")
 
 # Create sample data
 s <- c(offsetbio5, macrobio5)
@@ -27,20 +27,20 @@ set.seed(123)
 SampleOffsetbio5 <- spatSample(s, 100000, "regular", na.rm = TRUE, xy = TRUE)
 head(SampleOffsetbio5)
 colnames(SampleOffsetbio5)[3] <- "offsetBIO5"
-colnames(SampleOffsetbio5)[4] <- "chelsaMacroBIO5"
+colnames(SampleOffsetbio5)[4] <- "terraMacroBIO5"
 
 #### Check normal distribution####
 hist(SampleOffsetbio5$offsetBIO5)
-hist(SampleOffsetbio5$chelsaMacroBIO5)
+hist(SampleOffsetbio5$terraMacroBIO5)
 
 ########################
 #### Fit linear model####
 ########################
-reg1 <- lm(offsetBIO5 ~ chelsaMacroBIO5, data = SampleOffsetbio5)
+reg1 <- lm(offsetBIO5 ~ terraMacroBIO5, data = SampleOffsetbio5)
 summary(reg1)
 
 # Add the regression line
-ggplot(SampleOffsetbio5, aes(x = chelsaMacroBIO5, y = offsetBIO5)) +
+ggplot(SampleOffsetbio5, aes(x = terraMacroBIO5, y = offsetBIO5)) +
   geom_point() +
   geom_smooth(method = lm)
 
@@ -50,9 +50,9 @@ save(reg1,
 
 
 #### Load offset and macroclimate data of BIO6.
-offsetbio6 <- rast("E:/Output/ForestOffset/BIO5BIO6/OffsetBIO6_Current.tif")
+offsetbio6 <- rast("E:/Input/TerraClimate/OffsetBIO6_2000-2020.tif")
 macrobio6 <- rast(
-  "E:/Input/CHELSAdata/BIO5BIO6/1981-2010/1981-2010chelsaBIO6_EUforests_25m.tif"
+  "E:/Input/TerraClimate/2000-2020terraBIO6_EUforests_25m.tif"
 )
 plot(offsetbio6, main = "OffsetBIO6 1981-2010")
 plot(macrobio6, main = " CHELSA BIO6 1981-2010")
@@ -70,16 +70,16 @@ set.seed(23)
 SampleOffsetbio6 <- spatSample(sbio6, 100000, "regular", na.rm = TRUE, xy = TRUE)
 head(SampleOffsetbio6)
 colnames(SampleOffsetbio6)[3] <- "offsetBIO6"
-colnames(SampleOffsetbio6)[4] <- "chelsaMacroBIO6"
+colnames(SampleOffsetbio6)[4] <- "terraMacroBIO6"
 
 ########################
 #### Fit linear model####
 ########################
-reg2 <- lm(offsetBIO6 ~ chelsaMacroBIO6, data = SampleOffsetbio6)
+reg2 <- lm(offsetBIO6 ~ terraMacroBIO6, data = SampleOffsetbio6)
 summary(reg2)
 
 # Add the regression line
-ggplot(SampleOffsetbio6, aes(x = chelsaMacroBIO6, y = offsetBIO6)) +
+ggplot(SampleOffsetbio6, aes(x = terraMacroBIO6, y = offsetBIO6)) +
   geom_point() +
   geom_smooth(method = lm)
 
