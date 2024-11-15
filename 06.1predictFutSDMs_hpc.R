1d6191a7d3e2d12a95afe49743833ffa8d7841b8
XiaquZ <xiaqu.zhou@gmail.com>
2024-11-15 17:30:46 +0100

    Prepare for running future SDMs on hpc. Make tiles for all the predictors?.

---
+++ 06.1predictFutSDMs_hpc.R
@@ -0,0 +1,62 @@
+library(ENMeval)
+library(dplyr)
+library(enmSdmX)
+
+# Change the name of predictors.
+ForestClim_05 <- rast("E:/Output/SDM_test/bio5_belgium.tif")
+ForestClim_06 <- rast("E:/Output/SDM_test/bio6_belgium.tif")
+ForestClim_12 <- rast("E:/Output/SDM_test/bio12_belgium.tif")
+ForestClim_15 <- rast("E:/Output/SDM_test/bio15_belgium.tif")
+cec <- rast("E:/Output/SDM_test/cec_belgium.tif")
+clay <- rast("E:/Output/SDM_test/clay_belgium.tif")
+
+names(ForestClim_05) <- "ForestClim_05"
+names(ForestClim_06) <- "ForestClim_06"
+names(ForestClim_12) <- "ForestClim_12"
+names(ForestClim_15) <- "ForestClim_15"
+names(cec) <- "cec"
+names(clay) <- "clay"
+
+# Predict
+predictors <- c(
+    ForestClim_05, ForestClim_06,
+    ForestClim_12, ForestClim_15,
+    cec, clay
+)
+print(predictors)
+
+# Test on another species
+mdl02 <- load("E:/SDMs/Stef_SDMs/Models/Adoxa moschatellina.RData")
+mdl02 <- e.mx_rp.f
+
+# Select the best SDM based on delta AIC
+# This dplyr operation executes the sequential criteria explained above.
+res <- eval.results(mdl02)
+opt_mdl <- res[res$delta.AICc == 0, ]
+opt_mdl
+min_index <- which(res$delta.AICc == 0)
+mdl_select <- mdl02@models[[min_index]]
+
+species02 <- predictMaxNet(mdl_select, predictors, type = "logistic")
+plot(species02)
+species02 <- species02 * 100
+
+# Compare with the current distribution map
+adoxa_pre <- rast("E:/SDMs/Stef_SDMs/Maps_current/Adoxa moschatellina.tif")
+adoxa_pre <- crop(adoxa_pre, bel)
+adoxa_pre <- mask(adoxa_pre, bel)
+plot(adoxa_pre)
+adoxa_pre
+species02
+
+# Save outputs
+writeRaster(
+    species02,
+    "E:/Output/SDM_test/Adoxa moschatellina_2000-2020.tif",
+    overwrite = TRUE
+)
+writeRaster(
+    adoxa_pre,
+    "E:/Output/SDM_test/Adoxa moschatellina_2071-2100.tif",
+    overwrite = TRUE
+)
---
+++ MakeTiles_predictors.R
@@ -0,0 +1,24 @@
+library(terra)
+library(sf)
+bio5 <- rast("E:/Output/BIO5BIO6/future/PredictedMicroclimate_BIO5_2071-2100_ssp370.tif")
+bio6 <- rast("E:/Output/BIO5BIO6/future/PredictedMicroclimate_BIO6_2071-2100_ssp370.tif")
+bio12 <- rast("E:/Input/CHELSAdata/BIO12BIO15/2071-2100chelsaBIO12_EUforests_25m.tif")
+bio15 <- rast("E:/Input/CHELSAdata/BIO12BIO15/2071-2100chelsaBIO15_EUforests_25m.tif")
+cec <- rast("E:/Input/SDMs_Soil variables/cec.tif")
+clay <- rast("E:/Input/SDMs_Soil variables/clay.tif")
+
+#Read in data in sf format.
+c_shape <- read_sf("E:/EuropeShapefile/Shapefiles/Europe.shp") #change pathway if change to another country
+
+# create an initial grid for centroid determination
+c_grid <- st_make_grid(c_shape, cellsize = c(4e5, 4e5), square = TRUE) |>
+  st_as_sf()
+# inspect
+plot(st_geometry(c_shape))
+plot(st_geometry(c_grid), border = "red", add = TRUE)
+
+c_grid_spat <- vect(c_grid)
+
+## Making tiles:
+filename <- paste0("/lustre1/scratch/348/vsc34871/output/Tiles/FVoCC_50kmSR", "_.tif") #Change path
+tile <- makeTiles(FVoCC,c_grid_spat, filename, overwrite = TRUE)
