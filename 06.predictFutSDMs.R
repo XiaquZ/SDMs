e2dea10e01500f092feac464f11ee923a4ec6e85
XiaquZ <xiaqu.zhou@gmail.com>
2024-11-15 11:56:01 +0100

    Predict future distribution based on microclimate variables for Adoxa moschatellina.

--- 06.predictFutSDMs.R
+++ 06.predictFutSDMs.R
@@ -2,7 +2,6 @@ library(ENMeval)
 library(predicts)
 library(terra)
 library(dplyr)
-install.packages("enmSdmX")
 library(enmSdmX)
 # # Load the predictors
 # # BIO05, BIO06, BIO12,BIO15, cec, clay
@@ -25,9 +24,9 @@ library(enmSdmX)
 #     "E:/Input/SDMs_Soil variables/clay.tif"
 # )
 
-# # load the shapefile to crop a small region
-# bel <- vect("E:/EuropeShapefile/Shapefiles/Belgium.shp")
-# print(bel)
+# load the shapefile to crop a small region
+bel <- vect("E:/EuropeShapefile/Shapefiles/Belgium.shp")
+print(bel)
 
 # # Crop the predictors.
 # # BIO5
@@ -66,16 +65,16 @@ library(enmSdmX)
 #  "E:/Output/SDM_test/bio15_belgium.tif",
 #  overwrite = TRUE)
 
-# Two soil variables.
-# cation exchange capacity
-cec_bel <- crop(cec, bel)
-plot(cec_bel)
-cec_bel <- mask(cec_bel, bel)
-plot(cec_bel)
-writeRaster(cec_bel,
-    "E:/Output/SDM_test/cec_belgium.tif",
-    overwrite = TRUE
-)
+# # Two soil variables.
+# # cation exchange capacity
+# cec_bel <- crop(cec, bel)
+# plot(cec_bel)
+# cec_bel <- mask(cec_bel, bel)
+# plot(cec_bel)
+# writeRaster(cec_bel,
+#     "E:/Output/SDM_test/cec_belgium.tif",
+#     overwrite = TRUE
+# )
 
 # # soil clay content
 # clay_bel <- crop(clay, bel)
@@ -94,10 +93,20 @@ ForestClim_15 <- rast("E:/Output/SDM_test/bio15_belgium.tif")
 cec <- rast("E:/Output/SDM_test/cec_belgium.tif")
 clay <- rast("E:/Output/SDM_test/clay_belgium.tif")
 
-names(ForestClim_12)
-names(ForestClim_12) <- "mean annual precipitation 2071-2100"
-names(ForestClim_15) <- "precipitation seasonality 2071-2100"
-names(ForestClim_15)
+names(ForestClim_05) <- "ForestClim_05"
+names(ForestClim_06) <- "ForestClim_06"
+names(ForestClim_12) <- "ForestClim_12"
+names(ForestClim_15) <- "ForestClim_15"
+names(cec) <- "cec"
+names(clay) <- "clay"
+
+# Predict
+predictors <- c(
+    ForestClim_05, ForestClim_06,
+    ForestClim_12, ForestClim_15,
+    cec, clay
+)
+print(predictors)
 
 # Load one of the SDMs from stef
 # actaea spicata
@@ -118,19 +127,71 @@ min_index <- which(res$delta.AICc == min(res$delta.AICc))
 mdl_select <- mdl@models[[min_index]]
 str(mdl_select)
 
-# Predict
-predictors <- c(
-    ForestClim_05, ForestClim_06,
-    ForestClim_12, ForestClim_15,
-    cec, clay
-)
-predictors
+# actaea <- terra::predict(predictors, mdl_select, type = "logistic")
+# # Try another package
+# actaea <- predicts::predict(predictors, mdl_select, type = "logistic")
+# ?predicts::predict
+# ## The above two functions cannot run maxnet model.
 
-actaea <- terra::predict(predictors, mdl_select, type = "logistic")
-# Try another package
-actaea <- predicts::predict(predictors, mdl_select, type = "logistic")
-?predicts::predict
-actaea02 <- predictMaxNet(mdl_select, predictors[[1]], type = "logistic")
-class(predictors)
-?predicts::predict
-plot(predictors[[1]])
+# Try predictMaxNet function.
+actaea02 <- predictMaxNet(mdl_select, predictors, type = "logistic")
+plot(actaea02)
+
+# Compare with the current distribution map
+actaea_pre <- rast("E:/SDMs/Stef_SDMs/Maps_current/Actaea spicata.tif")
+actaea_pre <- crop(actaea_pre, bel)
+actaea_pre <- mask(actaea_pre, bel)
+actaea02 <- actaea02 * 100
+# Plot the two maps.
+par(mfrow = c(1, 2))
+plot(actaea_pre, main = "Suitability map for Actaea Spicata 2000-2020")
+plot(actaea02, main = "Suitability map for Actaea Spicata 2071-2100")
+par(mfrow = c(1, 1))
+# Save outputs
+writeRaster(
+    actaea_pre,
+    "E:/Output/SDM_test/Actaea Spicata_2000-2020.tif",
+    overwrite = TRUE
+)
+writeRaster(
+    actaea02,
+    "E:/Output/SDM_test/Actaea Spicata_2071-2100.tif",
+    overwrite = TRUE
+)
+?predictMaxNet
+
+# Test on another species
+mdl02 <- load("E:/SDMs/Stef_SDMs/Models/Adoxa moschatellina.RData")
+mdl02 <- e.mx_rp.f
+
+# Select the best SDM based on delta AIC
+# This dplyr operation executes the sequential criteria explained above.
+res <- eval.results(mdl02)
+opt_mdl <- res[res$delta.AICc == 0, ]
+opt_mdl
+min_index <- which(res$delta.AICc == 0)
+mdl_select <- mdl02@models[[min_index]]
+
+species02 <- predictMaxNet(mdl_select, predictors, type = "logistic")
+plot(species02)
+species02 <- species02 * 100
+
+# Compare with the current distribution map
+adoxa_pre <- rast("E:/SDMs/Stef_SDMs/Maps_current/Adoxa moschatellina.tif")
+adoxa_pre <- crop(adoxa_pre, bel)
+adoxa_pre <- mask(adoxa_pre, bel)
+plot(adoxa_pre)
+adoxa_pre
+species02
+
+# Save outputs
+writeRaster(
+    species02,
+    "E:/Output/SDM_test/Adoxa moschatellina_2000-2020.tif",
+    overwrite = TRUE
+)
+writeRaster(
+    adoxa_pre,
+    "E:/Output/SDM_test/Adoxa moschatellina_2071-2100.tif",
+    overwrite = TRUE
+)
